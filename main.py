05742562851eaff77cb656b595e7bee0fe119d44
tesrsrsf <jinzhenming392@gmail.com>
2024-10-16 19:45:40 +0900

    Removed testing instructions

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def twoSum(nums: List[int], target: int) -> List[int]:
                 return [i, j]
 
 
-print(twoSum([2, 7, 11, 15], 9))
-print(twoSum([3, 2, 4], 6))
-print(twoSum([3, 3], 6))
+#print(twoSum([2, 7, 11, 15], 9))
+#print(twoSum([3, 2, 4], 6))
+#print(twoSum([3, 3], 6))
